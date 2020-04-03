b84b0a29f349c748f5b988e5e245219663be2bd2
popo1206 <podchufarovapo2@gmail.com>
2020-04-03 13:34:24 +0300

    update 2.0

--- task1/PLY/lexerClass.py
+++ task1/PLY/lexerClass.py
@@ -3,23 +3,23 @@ import re
 
 class Lexer():
     states=(
-        ('name','exclusive'),
-        ('server','exclusive'),
-        ('zone','exclusive')
-    )
-    tokens=(
-        'MAILTO','NAME','SERVER','ZONE','NL','UNKNOWN','COMMA'
-    )
+            ('name','exclusive'),
+            ('server','exclusive'),
+            ('zone','exclusive')
+            )
+            tokens=(
+                    'MAILTO','NAME','SERVER','ZONE','NL','UNKNOWN','COMMA'
+                    )
+            
+            def __init__(self):
+                self.lexer=lex.lex(module=self)
 
-    def __init__(self):
-        self.lexer=lex.lex(module=self)
-
-    def input(self, data):
-        return self.lexer.input(data)
-
+def input(self, data):
+    return self.lexer.input(data)
+    
     def token(self):
         return self.lexer.token()
-
+    
     def t_MAILTO(self, t):
         r'^(?mi)mailto:'
         if t.lexer.current_state() == 'INITIAL':
@@ -27,44 +27,44 @@ class Lexer():
         else:
             t.lexer.begin('INITIAL')
         return t
-
+    
     def t_NL(self, t):
         r'(\n)'
         t.lexer.lineno+=len(t.value)
         t.lexer.begin('INITIAL')
         return t
-
+    
     def t_UNKNOWN(self, t):
         r'.+'
         return t
-
+    
     def t_COMMA(self,t):
         r'\s '
         return t
-
-
+    
+    
     def t_name_NAME(self,t):
         r'([a-zA-Z0-9]{1,20})'
         t.lexer.begin('server')
         return t
-
+    
     def t_name_NL(self, t):
         r'(\n)'
         t.lexer.lineno += len(t.value)
         t.lexer.begin('INITIAL')
         return t
-
+    
     def t_name_UNKNOWN(self, t):
         r'.+'
         t.lexer.begin('INITIAL')
         return t
-
-
+    
+    
     def t_server_SERVER(self,t):
         r'@[a-zA-Z0-9]{1,20}'
         t.lexer.begin('zone')
         return t
-
+    
     def t_server_NL(self, t):
         r'(\n)'
         t.lexer.lineno += len(t.value)
@@ -74,55 +74,40 @@ class Lexer():
         r'.+'
         t.lexer.begin('INITIAL')
         return t
-
+    
     def t_zone_ZONE(self,t):
         r'\.[a-zA-Z]{1,5}'
         t.lexer.begin('zone')
         return t
-
+    
     def t_zone_COMMA(self,t):
         r',\s'
         t.lexer.begin('name')
         return t
-
+    
     def t_zone_NL(self, t):
         r'(\n)'
         t.lexer.lineno += len(t.value)
         t.lexer.begin('INITIAL')
         return t
-
+    
     def t_zone_UNKNOWN(self, t):
         r'.+'
         t.lexer.begin('INITIAL')
         return t
-
+    
     def t_error(self, t):
         print("Illegal character '%s'" % t.value[0])
         t.lexer.begin('INITIAL')
-
+    
     def t_name_error(self, t):
         print("Illegal character in NAME '%s'" % t.value[0])
         t.lexer.begin('INITIAL')
-
+    
     def t_server_error(self, t):
         print("Illegal character in SERVER'%s'" % t.value[0])
         t.lexer.begin('INITIAL')
-
+    
     def t_zone_error(self, t):
         print("Illegal character in ZONE'%s'" % t.value[0])
         t.lexer.begin('INITIAL')
-
-if __name__ == '__main__':
-        f = open('task1.txt', 'r')
-        add = f.read()
-        f.close()
-        lexer = Lexer()
-        lexer.input(add)
-        # lexer.input('MAilTO:eanm89140@ssDUwdpcJCpUnAyOQEBwuirJMQoXYS.YEjDwjHGxqvSoTjtOLLrzdjPuuMFJdiMZpnfatGPCcBqSLLUqUnzPTyayVxH?subject=21710341519738066704318859566987860891621791630985757978783218')
-        while True:
-            tok = lexer.token()
-            print(tok)
-            if not tok:
-                break
-
-
